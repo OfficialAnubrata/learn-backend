5efe7561a22c57890a6cb8d6cd4eeb8a1307d2b8
OfficialAnubrata <anubrata.sarkar@iemlabs.com>
2024-02-27 12:51:08 +0530

    #

--- src/utils/asynchandeler.js
+++ src/utils/asynchandeler.js
@@ -1,26 +1,9 @@
 const  asyncHandeler = (requestHandeler) => {
-    (req,res,next) => {
-        Promise.resolve(
-            requestHandeler(req,res,next)
-        ).catch((err) => next(err))
+    return (req,res,next) => {
+        Promise.resolve(requestHandeler(req,res,next)).catch((error) => next(error))
     }
 }
 
 
 
-export {asyncHandeler}
-
-
-
-/*
-const asyncHandeler = (fn) => async(req,res,next) => {
-    try {
-        await fn(req,res,next)
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        })
-    }
-}
-*/
+export { asyncHandeler }
