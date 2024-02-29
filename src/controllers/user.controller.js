94fe0a65159c41ae45083ede829b0ed29140798b
OfficialAnubrata <anubrata.sarkar@iemlabs.com>
2024-02-29 12:42:09 +0530

    add others models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -150,8 +150,8 @@ const logoutUser = asyncHandeler(async(req, res)=> {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
         },
         {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -37,7 +37,7 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
-router.route("cover_image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
+router.route("/cover_image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT, getUserchannelprofile)
 router.route("/history").get(verifyJWT, getWatchHistory)
 
